9e5e8e6d9cce7875eec56c84dbccba1cded943f1
incipient-har <harish.e.incipient@gmail.com>
2021-05-28 23:27:09 +0530

    project initialised

---
+++ src/compnents/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+  return (
+    <div className='container text-center'>
+      <h1>Welcome The Dashboad!</h1>
+    </div>
+  );
+}
+
+export default index;
